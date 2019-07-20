e9743e68d8b583407e4b4427ec421b3192c1d861
zhongyuanjia <w278206624@gmail.com>
2019-07-21 01:06:31 +0800

    feat(logger): friendly error info

---
+++ src/logger.tool.ts
@@ -0,0 +1,17 @@
+import { ConfigurationError } from './error';
+
+export class LoggerTool {
+  private static readonly logger = console;
+
+  static log(level: string, message: string) {
+    LoggerTool.logger[level](`[nest-configuration ${level}]: ${message}`);
+  }
+
+  static warn(message: string) {
+    return LoggerTool.log('warn', message);
+  }
+
+  static error(message: string) {
+    throw new ConfigurationError(message);
+  }
+}
