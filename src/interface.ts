318aaf1a4dbd1604108fff6bfdcb0a388c87c807
zhongyuanjia <w278206624@gmail.com>
2019-07-21 01:19:16 +0800

    refactor(*): support security option, move env value to envVariable object
    security option can use for encrypt config value, and use decrypt function to decrypt it when app
    running
    
    BREAKING CHANGE: move all env value to envVariable object

--- src/interface.ts
+++ src/interface.ts
@@ -1,7 +1,36 @@
+export interface SecurityOptions {
+  /**
+   * custom decrypt function
+   * default using RSA
+   * @param encryptedValue
+   */
+  decrypt?: (encryptedValue: string) => string;
+  /**
+   * if true it will call decrypt function to decrypt it
+   * default: false
+   */
+  encrypted?: boolean;
+  /**
+   * RSA private key file path
+   * if encrypted option is true, you must specify this option
+   */
+  privateKeyFilePath?: string;
+}
+
+export interface ConfigurationOptions {
+  securityOptions?: SecurityOptions;
+}
+
 export interface EnvVariable<T = any> {
   base?: T;
   production?: T;
   development?: T;
   test?: T;
+
   [key: string]: T;
+}
+
+export interface ConfigOptions<T = any> {
+  envVariable: EnvVariable<T>;
+  configurationOptions?: ConfigurationOptions;
 }
