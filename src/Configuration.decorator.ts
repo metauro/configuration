0c70926ad3df5495ff17e9b367d068e7b1f4717c
zhongyuanjia <w278206624@gmail.com>
2019-07-21 01:29:13 +0800

    feat(configuration.decorator): support security options

--- src/Configuration.decorator.ts
+++ src/Configuration.decorator.ts
@@ -1,41 +1,77 @@
 import merge = require('lodash.merge');
 import { CONFIG_METADATA, env } from './constants';
-import { EnvVariable } from './interface';
+import { ConfigOptions, ConfigurationOptions } from './interface';
+import { SecurityTool } from './security.tool';
+import { LoggerTool } from './logger.tool';
+
+type PartialDepth<T> = T extends object
+  ? Partial<{ [K in keyof T]: PartialDepth<T[K]> }>
+  : T;
 
 export const Configuration = <T extends new (...arg: any[]) => any>(
-  target: T,
+  configurationOptions: ConfigurationOptions,
 ) => {
-  return class extends target {
-    constructor(...args: any[]) {
-      super(...args);
-      (Reflect.getMetadata(CONFIG_METADATA, target.prototype) || []).forEach(
-        property => {
-          const envVariable: EnvVariable = Reflect.getMetadata(
-            CONFIG_METADATA,
-            target.prototype,
-            property,
-          );
-          const propertyType = Reflect.getMetadata(
-            'design:type',
-            target.prototype,
-            property,
-          );
+  return (target: T) => {
+    return class extends target {
+      constructor(...args: any[]) {
+        super(...args);
+        (Reflect.getMetadata(CONFIG_METADATA, target.prototype) || []).forEach(
+          property => {
+            const {
+              envVariable,
+              configurationOptions: singleConfigurationOptions,
+            }: ConfigOptions = Reflect.getMetadata(
+              CONFIG_METADATA,
+              target.prototype,
+              property,
+            );
+            const propertyType = Reflect.getMetadata(
+              'design:type',
+              target.prototype,
+              property,
+            );
+            const {
+              securityOptions: { encrypted, privateKeyFilePath, decrypt },
+            } = merge<PartialDepth<ConfigurationOptions>, ConfigurationOptions>(
+              {
+                securityOptions: {
+                  encrypted: false,
+                  decrypt(val) {
+                    return SecurityTool.decrypt(val, privateKeyFilePath);
+                  },
+                },
+              },
+              singleConfigurationOptions || configurationOptions || {},
+            );
 
-          if (
-            propertyType === Number ||
-            propertyType === String ||
-            propertyType === Boolean ||
-            propertyType === Symbol ||
-            propertyType === Function ||
-            propertyType === null ||
-            propertyType === undefined
-          ) {
-            this[property] = envVariable[env] || envVariable.base;
-          } else {
-            this[property] = merge(envVariable[env], envVariable.base);
-          }
-        },
-      );
-    }
+            if (encrypted && propertyType !== String) {
+              LoggerTool.warn(
+                `only support encrypt string, please check ${target.name}.${property}`,
+              );
+            }
+
+            if (encrypted && propertyType === String) {
+              try {
+                this[property] = decrypt(envVariable[env] || envVariable.base);
+              } catch (err) {
+                LoggerTool.error(err);
+              }
+            } else if (
+              propertyType === Number ||
+              propertyType === String ||
+              propertyType === Boolean ||
+              propertyType === Symbol ||
+              propertyType === Function ||
+              propertyType === null ||
+              propertyType === undefined
+            ) {
+              this[property] = envVariable[env] || envVariable.base;
+            } else {
+              this[property] = merge(envVariable[env], envVariable.base);
+            }
+          },
+        );
+      }
+    };
   };
 };
