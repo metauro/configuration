d8bc8e53356eb71cf963e7815aa0efa788cda20c
zhongyuanjia <w278206624@gmail.com>
2019-07-21 01:25:49 +0800

    feat(config.decorator): allow override ConfigurationOptions

--- src/Config.decorator.ts
+++ src/Config.decorator.ts
@@ -1,14 +1,20 @@
-import { EnvVariable } from './interface';
+import { ConfigOptions } from './interface';
 import { CONFIG_METADATA } from './constants';
 
 export const Config: <T = any>(
-  envVariable: EnvVariable<T>,
-) => PropertyDecorator = envVariable => {
+  configOptions: ConfigOptions<T>,
+) => PropertyDecorator = ({ envVariable, configurationOptions }) => {
   return (target, propertyKey) => {
     if (!Reflect.getMetadata(CONFIG_METADATA, target)) {
       Reflect.defineMetadata(CONFIG_METADATA, [], target);
     }
     Reflect.getMetadata(CONFIG_METADATA, target).push(propertyKey);
     Reflect.defineMetadata(CONFIG_METADATA, envVariable, target, propertyKey);
+    Reflect.defineMetadata(
+      CONFIG_METADATA,
+      configurationOptions,
+      target,
+      propertyKey,
+    );
   };
 };
