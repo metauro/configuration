2e2daf44093ca31c59070936bc5be1d9637137ea
zhongyuanjia <w278206624@gmail.com>
2019-07-21 23:46:23 +0800

    refactor(security): add algorithms layer

--- src/tools/security.tool.ts
+++ src/tools/security.tool.ts
@@ -1,31 +1,11 @@
-import * as fs from 'fs';
-import * as NodeRSA from 'node-rsa';
-import { LoggerTool } from './logger.tool';
+import { RSA } from '../algorithms/RSA';
 
 export class SecurityTool {
   static encrypt(decryptedValue: string, publicKeyPath: string): string {
-    if (!fs.existsSync(publicKeyPath)) {
-      LoggerTool.error('public key file does not exists');
-    }
-
-    const publicKey = fs.readFileSync(publicKeyPath, 'utf8');
-    const rsa = new NodeRSA();
-    rsa.importKey(publicKey, 'public');
-    return rsa.encrypt(decryptedValue, 'utf8');
+    return RSA.encrypt(decryptedValue, publicKeyPath);
   }
 
   static decrypt(encryptedValue: string, privateKeyFilePath: string): string {
-    if (!privateKeyFilePath) {
-      LoggerTool.error('please special privateKeyFilePath');
-    }
-
-    if (!fs.existsSync(privateKeyFilePath)) {
-      LoggerTool.error('private key file does not exists');
-    }
-
-    const privateKey = fs.readFileSync(privateKeyFilePath, 'utf8');
-    const rsa = new NodeRSA();
-    rsa.importKey(privateKey, 'private');
-    return rsa.decrypt(encryptedValue, 'utf8');
+    return RSA.decrypt(encryptedValue, privateKeyFilePath);
   }
 }
