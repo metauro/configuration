55d376017184132a88900658117aaecbb10c9adf
zhongyuanjia <w278206624@gmail.com>
2019-07-22 14:01:12 +0800

    perf(interface): clean code

--- src/interface.ts
+++ src/interface.ts
@@ -1,4 +1,22 @@
+export interface ConfigurationOptions {
+  securityOptions?: SecurityOptions;
+  /**
+   * special current env
+   * default: process.env.NODE_ENV
+   */
+  env?: Env;
+}
+
+export interface ConfigOptions<T = any> {
+  envVariable: EnvVariable<T>;
+  /**
+   * override global configuration options
+   */
+  configurationOptions?: ConfigurationOptions;
+}
+
 export type Env = 'development' | 'production' | 'test' | string;
+export type EnvVariable<T = any> = { [key in Env]?: T };
 
 export interface SecurityOptions {
   /**
@@ -17,27 +35,4 @@ export interface SecurityOptions {
    * if encrypted option is true, you must specify this option
    */
   privateKeyFilePath?: string;
-}
-
-export interface ConfigurationOptions {
-  securityOptions?: SecurityOptions;
-  /**
-   * special current env
-   * default: process.env.NODE_ENV
-   */
-  env?: Env;
-}
-
-export interface EnvVariable<T = any> {
-  base?: T;
-  production?: T;
-  development?: T;
-  test?: T;
-
-  [key: string]: T;
-}
-
-export interface ConfigOptions<T = any> {
-  envVariable: EnvVariable<T>;
-  configurationOptions?: ConfigurationOptions;
 }
